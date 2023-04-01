51d2cf1dfeecdd633bdb6f7dd63f3016dc5633eb
albernalca <albernalca@gmail.com>
2023-04-01 00:29:59 -0500

    se crean variables, se crea metodo en ts y se realiza event binding en el html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Piano';
+  
+  titulo = 'Piano con Angular';
+  //creo variables
+  do = 'do'
+  re = 're'
+  mi = 'mi'
+  fa = 'fa'
+  sol = 'sol'
+  la = 'la'
+  si = 'si'
+  
+  //creo metodo pasando parámetro texto
+  aplicarSonido(texto: string): void {
+    //declaro constante audio y la instancio
+    const audio = new Audio()
+    //ejecuto la ruta de los sonidos y concateno el texto
+    audio.src = 'assets/sonidos/' + texto + '.wav'
+    //instrucción para que cargue el audio
+    audio.load()
+    //intrucción para que lo ejecute
+    audio.play()
+
+  }
+
+
 }
